bb8ec7941bb3ab5be8bb2650765a731471319c12
gas8542 <142408699+gas8542@users.noreply.github.com>
2025-08-10 13:54:24 +0300

    Add files via upload

--- мусор/PhysiCalcJS.js
+++ мусор/PhysiCalcJS.js
@@ -1,2 +1,10 @@
-const inputElement = document.querySelectorAll(".formul-window");
-console.log(inputElement)
+document.addEventListener('DOMContentLoaded', function() {
+    const window_input_bad = document.getElementById("searchBox");
+  
+    if (window_input_bad) {
+      window_input_bad.addEventListener('input', function() {
+        const window_input = window_input_bad.value;
+        console.log(window_input);
+      });
+    }
+  });
