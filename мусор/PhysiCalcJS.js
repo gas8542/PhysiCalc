c1d484ab6ca354666d53e86f8347ed5993e04efa
gas8542 <142408699+gas8542@users.noreply.github.com>
2025-08-13 11:02:15 +0300

    Add files via upload

--- мусор/PhysiCalcJS.js
+++ мусор/PhysiCalcJS.js
@@ -8,3 +8,48 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     }
   });
+
+ function mehJS() {
+  const meh_swap_Elements_1 = document.getElementById('meh_swap_Elements');
+  console.log('braaa');
+  meh_swap_Elements_1.scrollIntoView({
+    behavior:"smooth",
+    block: "center"
+  }); 
+};
+
+function mktJS() {
+  const mkt_swap_Elements_1 = document.getElementById('mkt_swap_Elements');
+  console.log('braaa');
+  mkt_swap_Elements_1.scrollIntoView({
+    behavior:"smooth",
+    block: "center"
+  }); 
+};
+
+function electrostatics_swap_ElementsJS() {
+  const electrostatics_swap_Elements_1 = document.getElementById('electrostatics_swap_Elements');
+  console.log('braaa');
+  electrostatics_swap_Elements_1.scrollIntoView({
+    behavior:"smooth",
+    block: "center"
+  }); 
+};
+
+function DC_swap_ElementsJS() {
+  const DC_swap_Elements_1 = document.getElementById('DC_swap_Elements');
+  console.log('braaa');
+  DC_swap_Elements_1.scrollIntoView({
+    behavior:"smooth",
+    block: "center"
+  }); 
+};
+
+function bylerwbz_swap_ElementsJS() {
+  const bylerwbz_swap_Elements_1 = document.getElementById('bylerwbz_swap_Elements');
+  console.log('braaa');
+  bylerwbz_swap_Elements_1.scrollIntoView({
+    behavior:"smooth",
+    block: "center"
+  }); 
+};
